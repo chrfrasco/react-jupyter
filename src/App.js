43e677e76fa6338de7c294653abec133f37953de
eoinmurray <amonecho1@gmail.com>
2017-03-26 18:56:11 +0200

    first deploy of website

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Jupyter from 'react-jupyter'
-import logo from './logo.svg';
-import './App.css';
+import request from 'superagent'
+import logo from './logo.svg'
+import notebookUrl from './index.ipynb'
+import './App.css'
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = { notebookJSON: null }
+  }
+
+  componentDidMount(){
+    request.get(notebookUrl, (err, res) => {
+      this.setState({notebookJSON: JSON.parse(res.text)})
+    })
+  }
+
   render() {
+    const { notebookJSON } = this.state
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="page-content">
+        <div className="wrapper">
+          <div className="post">
+            <div className="post-header">
+              <strong className="post-title">Fitting a Gaussian to spectral data</strong>
+              <div className="post-info">
+                notebook.ipynb
+              </div>
+              <time>Mar 7, 2017</time>
+            </div>
+            <hr />
+            { notebookJSON &&
+              <Jupyter
+                notebook={notebookJSON}
+                defaultStyle
+                showCode
+              />
+            }
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
