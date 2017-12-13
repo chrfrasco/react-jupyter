f08f4c1b2bde38bfcd9a95cd5d91b85c82692afe
Christian Scott <christianfscott@gmail.com>
2017-12-13 15:41:04 +1300

    Add showError tests

--- src/__tests__/index-test.js
+++ src/__tests__/index-test.js
@@ -25,3 +25,13 @@ it('showCode=false prevents rendering of code blocks', () => {
   const wrapper = mount(<Notebook showCode={false} />)
   expect(wrapper.find(CODE_SELECTOR).length).toEqual(0)
 })
+
+it('renders error cells by default', () => {
+  const wrapper = mount(<Notebook notebookJson={errorNotebookJson} />)
+  expect(wrapper.find(ERR_SELECTOR).length).toEqual(1)
+})
+
+it('showError=false prevents rendering of error blocks', () => {
+  const wrapper = mount(<Notebook notebookJson={errorNotebookJson} showError={false} />)
+  expect(wrapper.find(ERR_SELECTOR).length).toEqual(0)
+})
