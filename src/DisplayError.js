641fdffb3dd17b94e472c4e654734f3dc8110de6
Christian Scott <christianfscott@gmail.com>
2017-12-13 15:17:45 +1300

    Update ansi_up usage in DisplayError

--- src/DisplayError.js
+++ src/DisplayError.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import Code from './Code'
-import ansi_up from 'ansi_up'
+import AnsiUp from 'ansi_up'
 
 class DisplayError extends Component {
   render() {
     const { raw } = this.props
     const text = raw.traceback.join('\n')
+    const ansi_up = new AnsiUp()
 
     return <Code className="pyerr" string={ansi_up.ansi_to_html(text)} />
   }
